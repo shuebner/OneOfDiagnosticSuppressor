a10fc3df1093500c9b5a82a8083ab8095ae813b6
Sven Hübner <sven.huebner.it@gmx.de>
2022-09-13 18:18:21 +0200

    extract logic for determining if a type has OneOf guarantees

---
+++ OneOfDiagnosticSuppressor/OneOfTypeHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SvSoft.OneOf.Analyzers.SwitchDiagnosticSuppression;
+static class OneOfTypeHelper
+{
+    public static IEnumerable<INamedTypeSymbol>? GetOneOfSubTypes(INamedTypeSymbol oneOfCandidate)
+    {
+        if (oneOfCandidate.Name.Equals("OneOf", StringComparison.Ordinal) &&
+            oneOfCandidate.ContainingAssembly.Name.Equals("OneOf", StringComparison.Ordinal))
+        {
+            var namedTypeArguments = oneOfCandidate.TypeArguments.OfType<INamedTypeSymbol>().ToArray();
+            if (namedTypeArguments.Length == oneOfCandidate.TypeArguments.Length)
+            {
+                return namedTypeArguments;
+            }
+        }
+
+        return null;
+    }
+}
