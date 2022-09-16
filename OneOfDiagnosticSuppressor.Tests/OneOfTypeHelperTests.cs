a0573021b47cd2a556faee6c397125cdd1ffa1f0
Sven Hübner <sven.huebner.it@gmx.de>
2022-09-16 17:30:42 +0200

    suppress on OneOfBase

--- OneOfDiagnosticSuppressor.Tests/OneOfTypeHelperTests.cs
+++ OneOfDiagnosticSuppressor.Tests/OneOfTypeHelperTests.cs
@@ -39,6 +39,106 @@ class OneOfTypeHelperTests
     }
 
     [Test]
+    [TestCase("bool")]
+    [TestCase("bool, string")]
+    [TestCase("bool?, string")]
+    [TestCase("bool, string?")]
+    [TestCase("object, object?, object")]
+    public void When_type_is_a_OneOfBase_Then_returns_subtypes(string oneOfBaseTypeParameterList)
+    {
+        var oneOfType = $"OneOf.OneOfBase<{oneOfBaseTypeParameterList}>";
+        var dummyParameterList = string.Join(", ", oneOfBaseTypeParameterList.Split(", ").Select((type, index) => $"{type} t{index}"));
+        var code = WrapInNamespace($@"static class Foo {{ public static void Do({oneOfType} instance, {dummyParameterList}) {{ }} }}");
+        Compilation compilation = CompilationHelper.CreateCompilation(code);
+        IReadOnlyList<INamedTypeSymbol> parameterTypes = compilation.GetSymbolsWithName("Do")
+            .OfType<IMethodSymbol>()
+            .Single().Parameters
+            .Select(p => p.Type)
+            .OfType<INamedTypeSymbol>()
+            .ToArray();
+
+        INamedTypeSymbol instanceType = parameterTypes.First();
+        var expectedSubtypes = parameterTypes.Skip(1);
+
+        var subtypes = OneOfTypeHelper.GetOneOfSubTypes(instanceType);
+
+        Assert.IsNotNull(subtypes);
+        Assert.IsTrue(
+            subtypes!.SequenceEqual(expectedSubtypes, SymbolEqualityComparer.IncludeNullability),
+            "got {0} but expected {1}",
+            string.Join(", ", subtypes!),
+            string.Join(", ", expectedSubtypes));
+    }
+
+    [Test]
+    [TestCase("bool")]
+    [TestCase("bool, string")]
+    [TestCase("bool?, string")]
+    [TestCase("bool, string?")]
+    [TestCase("object, object?, object")]
+    public void When_type_inherits_directly_from_OneOfBase_Then_returns_subtypes(string oneOfBaseTypeParameterList)
+    {
+        var dummyParameterList = string.Join(", ", oneOfBaseTypeParameterList.Split(", ").Select((type, index) => $"{type} t{index}"));
+        var code = WrapInNamespace($@"
+class InheritedOneOfBase : OneOf.OneOfBase<{oneOfBaseTypeParameterList}> {{ }}
+static class Foo {{ public static void Do(InheritedOneOfBase instance, {dummyParameterList}) {{ }} }}");
+        Compilation compilation = CompilationHelper.CreateCompilation(code);
+        IReadOnlyList<INamedTypeSymbol> parameterTypes = compilation.GetSymbolsWithName("Do")
+            .OfType<IMethodSymbol>()
+            .Single().Parameters
+            .Select(p => p.Type)
+            .OfType<INamedTypeSymbol>()
+            .ToArray();
+
+        INamedTypeSymbol instanceType = parameterTypes.First();
+        var expectedSubtypes = parameterTypes.Skip(1);
+
+        var subtypes = OneOfTypeHelper.GetOneOfSubTypes(instanceType);
+
+        Assert.IsNotNull(subtypes);
+        Assert.IsTrue(
+            subtypes!.SequenceEqual(expectedSubtypes, SymbolEqualityComparer.IncludeNullability),
+            "got {0} but expected {1}",
+            string.Join(", ", subtypes!),
+            string.Join(", ", expectedSubtypes));
+    }
+
+    [Test]
+    [TestCase("bool")]
+    [TestCase("bool, string")]
+    [TestCase("bool?, string")]
+    [TestCase("bool, string?")]
+    [TestCase("object, object?, object")]
+    public void When_type_inherits_indirectly_from_OneOfBase_Then_returns_subtypes(string oneOfBaseTypeParameterList)
+    {
+        var dummyParameterList = string.Join(", ", oneOfBaseTypeParameterList.Split(", ").Select((type, index) => $"{type} t{index}"));
+        var code = WrapInNamespace($@"
+class InheritedOneOfBase : OneOf.OneOfBase<{oneOfBaseTypeParameterList}> {{ }}
+class OtherInheritanceLevel : InheritedOneOfBase {{ }}
+class FinalClass : OtherInheritanceLevel {{ }}
+static class Foo {{ public static void Do(FinalClass instance, {dummyParameterList}) {{ }} }}");
+        Compilation compilation = CompilationHelper.CreateCompilation(code);
+        IReadOnlyList<INamedTypeSymbol> parameterTypes = compilation.GetSymbolsWithName("Do")
+            .OfType<IMethodSymbol>()
+            .Single().Parameters
+            .Select(p => p.Type)
+            .OfType<INamedTypeSymbol>()
+            .ToArray();
+
+        INamedTypeSymbol instanceType = parameterTypes.First();
+        var expectedSubtypes = parameterTypes.Skip(1);
+
+        var subtypes = OneOfTypeHelper.GetOneOfSubTypes(instanceType);
+
+        Assert.IsNotNull(subtypes);
+        Assert.IsTrue(
+            subtypes!.SequenceEqual(expectedSubtypes, SymbolEqualityComparer.IncludeNullability),
+            "got {0} but expected {1}",
+            string.Join(", ", subtypes!),
+            string.Join(", ", expectedSubtypes));
+    }
+
+    [Test]
     [TestCase("OneOf.IOneOf")]
     [TestCase("OtherNamespace.OneOf<bool>")]
     public void When_type_is_not_a_OneOf_Then_returns_null(string notOneOfType)
--- OneOfDiagnosticSuppressor/OneOfTypeHelper.cs
+++ OneOfDiagnosticSuppressor/OneOfTypeHelper.cs
@@ -6,11 +6,12 @@ using System.Linq;
 namespace SvSoft.OneOf.Analyzers.SwitchDiagnosticSuppression;
 static class OneOfTypeHelper
 {
+    static readonly string[] OneOfTypeNames = { "OneOf", "OneOfBase" };
+
     public static IEnumerable<INamedTypeSymbol>? GetOneOfSubTypes(INamedTypeSymbol oneOfCandidate)
     {
-        if (oneOfCandidate.Name.Equals("OneOf", StringComparison.Ordinal) &&
-            oneOfCandidate.ContainingAssembly.Name.Equals("OneOf", StringComparison.Ordinal)
-            )
+        if (OneOfTypeNames.Any(name => oneOfCandidate.Name.Equals(name, StringComparison.Ordinal) &&
+            oneOfCandidate.ContainingAssembly.Name.Equals("OneOf", StringComparison.Ordinal)))
         {
             var namedTypeArguments = oneOfCandidate.TypeArguments.OfType<INamedTypeSymbol>().ToArray();
             if (namedTypeArguments.Length == oneOfCandidate.TypeArguments.Length)
@@ -19,6 +20,28 @@ static class OneOfTypeHelper
             }
         }
 
+        if (TryGetRootType(oneOfCandidate) is INamedTypeSymbol baseType &&
+            baseType.Name.Equals("OneOfBase", StringComparison.Ordinal))
+        {
+            var namedTypeArguments = baseType.TypeArguments.OfType<INamedTypeSymbol>().ToArray();
+            if (namedTypeArguments.Length == baseType.TypeArguments.Length)
+            {
+                return namedTypeArguments;
+            }
+        }
+
         return null;
+
+        INamedTypeSymbol? TryGetRootType(INamedTypeSymbol typeSymbol)
+        {
+            INamedTypeSymbol rootType = typeSymbol;
+            while (rootType.BaseType is not null &&
+                   rootType.BaseType.SpecialType is not SpecialType.System_Object)
+            {
+                rootType = rootType.BaseType;
+            }
+
+            return rootType;
+        }
     }
 }
